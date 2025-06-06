73fbeee7498be21675b0e71b5f706fef768618dc
Saisreevenkat <saisreevenkat.p.14.08.2000@gmail.com>
2025-06-06 16:30:23 -0700

    Update setup_text_index.py

--- setup_text_index.py
+++ setup_text_index.py
@@ -3,4 +3,4 @@ from config import MONGO_URI
 
 db = MongoClient(MONGO_URI)["research_papers"]
 db["article_text"].create_index("pubmed_id", unique=True)
-print("✅  Unique index on article_text.pubmed_id created.")
+print("Unique index on article_text.pubmed_id created.")
